856740d7b53d8a4326aad98649f06520865116ad
Frank Nasseri <franknasseri57@gmail.com>
2017-07-06 23:40:46 -0400

    adding homework to loops

--- LoopsPracticeSummer17/Program.cs
+++ LoopsPracticeSummer17/Program.cs
@@ -221,33 +221,45 @@ namespace LoopsPracticeSummer17
 
             //Remember that in FizzBuzz, if it is not a FizzBuzz, Fizz, or Buzz number
             //you print the number itself.
-            Console.WriteLine("Let's Play FizzBuzz!");
-            Console.WriteLine("Please enter your FIZZ number");
-            int fizz = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter your BUZZ number");
-            int buzz = int.Parse(Console.ReadLine());
-            Console.WriteLine("What number would you like me to count to? (Pick a high one)");
-            int endNumber = int.Parse(Console.ReadLine());
-            for (int i = 0; i <= endNumber; i++)
+            //Console.WriteLine("Let's Play FizzBuzz!");
+            //Console.WriteLine("Please enter your FIZZ number");
+            //int fizz = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please enter your BUZZ number");
+            //int buzz = int.Parse(Console.ReadLine());
+            //Console.WriteLine("What number would you like me to count to? (Pick a high one)");
+            //int endNumber = int.Parse(Console.ReadLine());
+            //for (int i = 0; i <= endNumber; i++)
+            //{
+            //    if (i % fizz == 0 && i % buzz == 0)
+            //    {
+            //        Console.WriteLine("Fizz Buzz!");
+            //    }
+            //    else if (i % fizz == 0)
+            //    {
+            //        Console.WriteLine("Fizz!");
+            //    }
+            //    else if (i % buzz == 0)
+            //    {
+            //        Console.WriteLine("Buzz!");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //create an array of lucky numbers (#s you like). using a foreach loop, print the following
+            //Your Lucky Number is: #
+            //Your Lucky Number is: # etc, etc
+            int[] luckyNumbers = { 4, 7, 11, 12, 57 };
+            foreach (int number in luckyNumbers)
             {
-                if (i % fizz == 0 && i % buzz == 0)
-                {
-                    Console.WriteLine("Fizz Buzz!");
-                }
-                else if (i % fizz == 0)
-                {
-                    Console.WriteLine("Fizz!");
-                }
-                else if (i % buzz == 0)
-                {
-                    Console.WriteLine("Buzz!");
-                }
-                else
-                {
-                    Console.WriteLine(i);
-                }
+                Console.WriteLine("Your Lucky Number is: " +  (number));
             }
 
+
+            
+           
 
         }
     }
